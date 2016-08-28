413d9c3b271ea3667ae37c21c747c19a70eb4144
JulianDomingo <julian.domingo@utexas.edu>
2016-08-28 18:16:58 -0500

    Update emailphone.py

--- emailphone.py
+++ emailphone.py
@@ -4,18 +4,18 @@
 import sys, re, pyperclip
 
 phone = re.compile(r'''
-	(\d{3}|\(\d{3}\))															# area code
-	([- ])?																	# optional separator
-	(\d{3})																	# phone number part 1
-	([- ])?																	# optional separator
-	(\d{4})																	# phone number part 2
+	(\d{3}|\(\d{3}\))											# area code
+	([- ])?													# optional separator
+	(\d{3})													# phone number part 1
+	([- ])?													# optional separator
+	(\d{4})													# phone number part 2
 ''', re.VERBOSE | re.DOTALL)
 
 email = re.compile(r'''(
-	[a-zA-Z0-9\.]+																# email must be at least one character
-	@																	# must have this to be an email
-    [a-zA-Z0-9]+																# domain name
-	\.[a-zA-Z]{2,4}																# email must end with one of these options
+	[a-zA-Z0-9\.]+												# email must be at least one character
+	@													# must have this to be an email
+    [a-zA-Z0-9]+												# domain name
+	\.[a-zA-Z]{2,4}												# email must end with one of these options
 )''', re.VERBOSE)
 
 
@@ -27,8 +27,8 @@ while True:
 	if choice.lower() == 'phone':
 		numbers = phone.findall(text)
 		for index in range(0, len(numbers)):
-			numbers[index] = ''.join(numbers[index])										# joins together the strings in each tuple in the list of tuples
-		pyperclip.copy('\n'.join(numbers))												# joins together each index of list with a newline
+			numbers[index] = ''.join(numbers[index])						# joins together the strings in each tuple in the list of tuples
+		pyperclip.copy('\n'.join(numbers))								# joins together each index of list with a newline
 		print('Copied all phone numbers. Press any key to exit.')
 		sys.exit()
 
