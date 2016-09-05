bc92cbe11e8c99946589cdd4598a558931f63f3c
JulianDomingo <julian.domingo@utexas.edu>
2016-09-04 23:09:02 -0500

    Update regexSearch.py

--- regexSearch.py
+++ regexSearch.py
@@ -1,4 +1,4 @@
-#! python 3
+#! python3
 # opens all .txt files in a folder (specified by user) and
 # searches for any line that matches a regex (also specified
 # by user)
@@ -12,10 +12,12 @@ os.chdir(input())
 txtFiles = txtFinder.findall(', '.join(os.listdir()))							# finds all .txt files and stores in list
 
 print('Enter regular expression for searching .txt files:')
-print("Format: r'<expression>'")
+print("Format: <expression>")
 userRegex = re.compile(input())													# create user-specified regex
 
-for file in txtFiles:
-	print(', '.join(userRegex.findall(file)))									# find all occurrences and convert to str before output to console
+print(os.listdir())
 
+for file in txtFiles:
+	content = open(file, 'r')
+	print(', '.join(userRegex.findall(content.read())))									# find all occurrences and convert to str before output to console
 
